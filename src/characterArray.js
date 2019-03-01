742f02cf15c564d60af0734826e6014e72b5477d
nevermindthelabel <nevermindthelabel@gmail.com>
2019-03-01 07:25:47 -0700

    created initial array of Simpsons characters

---
+++ src/characterArray.js
@@ -0,0 +1,137 @@
+export default [
+  {
+    "id": 1,
+    "image": "https://static.simpsonswiki.com/images/a/a9/Abraham_Simpson.png",
+    "name": "Grandpa Simpson"
+  },
+  {
+    "id": 2,
+    "image": "https://static.simpsonswiki.com/images/6/65/Bart_Simpson.png",
+    "name": "Bart Simpson"
+  },
+  {
+    "id": 3,
+    "image": "https://static.simpsonswiki.com/images/9/9d/Charles_Montgomery_Burns.png",
+    "name": "Mr Burns"
+  },
+  {
+    "id": 4,
+    "image": "https://static.simpsonswiki.com/images/9/9d/Comic_Book_Guy.png",
+    "name": "Comic Book Guy"
+  },
+  {
+    "id": 5,
+    "image": "https://static.simpsonswiki.com/images/f/f7/Disco_Stu.png",
+    "name": "Disco Stu"
+  },
+  {
+    "id": 6,
+    "image": "https://static.simpsonswiki.com/images/a/a6/Dolph_Starbeam.png",
+    "name": "Dolph"
+  },
+  {
+    "id": 7,
+    "image": "https://static.simpsonswiki.com/images/1/11/Drederick_Tatum.png",
+    "name": "Drederic Tatum"
+  },
+  {
+    "id": 8,
+    "image": "https://static.simpsonswiki.com/images/e/ed/Duffman.png",
+    "name": "Duffman"
+  },
+  {
+    "id": 9,
+    "image": "https://static.simpsonswiki.com/images/f/f9/Gary_Chalmers.png",
+    "name": "Superintendent Chalmers"
+  },
+  {
+    "id": 10,
+    "image": "https://static.simpsonswiki.com/images/b/bd/Homer_Simpson.png",
+    "name": "Homer Simpson"
+  },
+  {
+    "id": 11,
+    "image": "https://static.simpsonswiki.com/images/4/42/Jimbo_Jones.png",
+    "name": "Jimbo"
+  },
+  {
+    "id": 12,
+    "image": "https://static.simpsonswiki.com/images/9/9c/Julius_Hibbert.png",
+    "name": "Dr Hibbert"
+  },
+  {
+    "id": 13,
+    "image": "https://static.simpsonswiki.com/images/6/64/Kearney_Zzyzwicz.png",
+    "name": "Kearney"
+  },
+  {
+    "id": 14,
+    "image": "https://static.simpsonswiki.com/images/e/ec/Lisa_Simpson.png",
+    "name": "Lisa Simpson"
+  },
+  {
+    "id": 15,
+    "image": "https://static.simpsonswiki.com/images/9/9d/Maggie_Simpson.png",
+    "name": "Maggie Simpson"
+  },
+  {
+    "id": 16,
+    "image": "https://static.simpsonswiki.com/images/0/0b/Marge_Simpson.png",
+    "name": "Marge Simpson"
+  },
+  {
+    "id": 17,
+    "image": "https://static.simpsonswiki.com/images/8/80/Moe_Szyslak.png",
+    "name": "Moe Szylak"
+  },
+  {
+    "id": 18,
+    "image": "https://static.simpsonswiki.com/images/8/84/Ned_Flanders.png",
+    "name": "Ned Flanders"
+  },
+  {
+    "id": 19,
+    "image": "https://static.simpsonswiki.com/images/c/c5/Nelson_Muntz.png",
+    "name": "Nelson"
+  },
+  {
+    "id": 20,
+    "image": "https://static.simpsonswiki.com/images/0/05/Nick_Riviera.png",
+    "name": "Dr Nick"
+  },
+  {
+    "id": 21,
+    "image": "https://static.simpsonswiki.com/images/0/09/Otto_Mann.png",
+    "name": "Otto Mann"
+  },
+  {
+    "id": 22,
+    "image": "https://static.simpsonswiki.com/images/7/7a/Plopper.png",
+    "name": "Spider Pig"
+  },
+  {
+    "id": 23,
+    "image": "https://static.simpsonswiki.com/images/3/3a/Seymour_Skinner.png",
+    "name": "Principal Skinner"
+  },
+  {
+    "id": 24,
+    "image": "https://static.simpsonswiki.com/images/8/80/Rod_Flanders.png",
+    "name": "Rod Flanders"
+  },
+  {
+    "id": 25,
+    "image": "https://static.simpsonswiki.com/images/1/18/Todd_Flanders.png",
+    "name": "Todd Flanders"
+  },
+  {
+    "id": 26,
+    "image": "https://static.simpsonswiki.com/images/f/f7/Troy_McClure.png",
+    "name": "Troy McClure"
+  },
+  {
+    "id": 27,
+    "image": "https://static.simpsonswiki.com/images/5/5d/Waylon_Smithers%2C_Jr..png",
+    "name": "Smithers"
+  }
+]
