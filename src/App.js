07615555fa3f2f56b976c3c459834414c7df8b40
nevermindthelabel <nevermindthelabel@gmail.com>
2019-03-02 09:36:05 -0700

    added shuffleArray

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import CharacterCard from "./components/CharacterCard";
-import Wrapper from './components/Wrapper'
-import Title from './components/Title'
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
 import "./App.css";
 import characterArray from "./characterArray";
 
@@ -10,42 +10,60 @@ export default class App extends Component {
     characterArray
   };
 
-  /* TODO
-    get a random id number to be the target id
-    compare target id number with the clicked id number
-    if it is not the target id, increment user score
-    shuffle characterArray onClick if it is not the target id
-  */ 
+  componentDidMount() {
+    this.shuffleArray()
+  }
 
-  // shuffleArray = characterArray => {
-  //     let i = characterArray.length - 1;
-  //     for (; i > 0; i--) {
-  //       const j = Math.floor(Math.random() * (i + 1));
-  //       const temp = characterArray[i];
-  //       characterArray[i] = characterArray[j];
-  //       characterArray[j] = temp;
-  //     }
-  //     this.setState({ characterArray })
-  //     // return array;
-  //   }
- 
+  /* TODO
+    
+    images get shuffled
+    images get displayed
+    score = 0
+    topScore = 0
+    if user clicks an image
+    the...
+    the score goes up by 1
+
+    shuffle image array
+    display images
+    update displayed score
+    if score > topScore
+    ... then increase topScore by 1
+    if user clicks the same image twice
+    then....
+    set score to 0
+
+  */
+
+  shuffleArray = () => {
+      let lastIndex = characterArray.length - 1;
+      for (; lastIndex > 0; lastIndex--) {
+        const randomIndex = Math.floor(Math.random() * (lastIndex + 1));
+        const temp = characterArray[lastIndex];
+        characterArray[lastIndex] = characterArray[randomIndex];
+        characterArray[randomIndex] = temp;
+      }
+      this.setState({ characterArray })
+    }
 
   render() {
     return (
       <Wrapper>
-        <Title>The Simpsons Characters</Title>
-          {this.state.characterArray.map(character => (
-            <CharacterCard
-              shuffleArray={this.shuffleArray}
-              id={character.id}
-              key={character.id}
-              image={character.image}
-              name={character.name}
-              onClick={() => console.log(`clicked`)}
-              thisId={() => console.log(character.id)}
-            />
-          ))}
-    </Wrapper>
+        <Title>The Simpsons Characters
+        <p>score 0 Top score 0</p>
+          </Title> 
+        {this.state.characterArray.map(character => (
+          <CharacterCard
+            shuffleArray={this.shuffleArray}
+            id={character.id}
+            key={character.id}
+            image={character.image}
+            name={character.name}
+            onClick={() => console.log(`clicked`)}
+            thisId={() => console.log(character.id)}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
