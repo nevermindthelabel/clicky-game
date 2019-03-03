b061547073be5a1e3ba7f5c7b103ff01890b084e
nevermindthelabel <nevermindthelabel@gmail.com>
2019-03-02 20:46:02 -0700

    added logic to update score and top score

--- src/App.js
+++ src/App.js
@@ -5,65 +5,66 @@ import Title from "./components/Title";
 import "./App.css";
 import characterArray from "./characterArray";
 
+let maxScore = 0;
+
 export default class App extends Component {
   state = {
-    characterArray
+    characterArray,
+    score: 0
+    // maxScore: 0
   };
 
-  componentDidMount() {
-    this.shuffleArray()
-  }
+  // componentDidMount() {
+  //   this.shuffleArray()
+  // }
 
-  /* TODO
-    
-    images get shuffled
-    images get displayed
-    score = 0
-    topScore = 0
-    if user clicks an image
-    the...
-    the score goes up by 1
-
-    shuffle image array
-    display images
-    update displayed score
-    if score > topScore
-    ... then increase topScore by 1
-    if user clicks the same image twice
-    then....
-    set score to 0
-
-  */
-
-  shuffleArray = () => {
-      let lastIndex = characterArray.length - 1;
-      for (; lastIndex > 0; lastIndex--) {
-        const randomIndex = Math.floor(Math.random() * (lastIndex + 1));
-        const temp = characterArray[lastIndex];
-        characterArray[lastIndex] = characterArray[randomIndex];
-        characterArray[randomIndex] = temp;
+  handleIncrement = () => {
+    this.setState({ score: this.state.score + 1 })
+      if (this.state.score >= maxScore) {
+        // this.setState({ maxScore: this.state.maxScore + 1 })
+        maxScore++
       }
-      this.setState({ characterArray })
-    }
+  } 
 
+  startGame = () => {
+    this.setState({ score: this.state.score })
+  }
+  
+  shuffleArray = () => {
+    this.handleIncrement();
+    let lastIndex = characterArray.length - 1;
+    for (; lastIndex > 0; lastIndex--) {
+      const randomIndex = Math.floor(Math.random() * (lastIndex + 1));
+      const temp = characterArray[lastIndex];
+      characterArray[lastIndex] = characterArray[randomIndex];
+      characterArray[randomIndex] = temp;
+    }
+    this.setState({ characterArray })
+  }
+  
   render() {
     return (
       <Wrapper>
         <Title>The Simpsons Characters
-        <p>score 0 Top score 0</p>
+        <p>score: {this.state.score} Top score: {maxScore} </p>
           </Title> 
         {this.state.characterArray.map(character => (
           <CharacterCard
-            shuffleArray={this.shuffleArray}
-            id={character.id}
-            key={character.id}
-            image={character.image}
-            name={character.name}
-            onClick={() => console.log(`clicked`)}
-            thisId={() => console.log(character.id)}
+          shuffleArray={this.shuffleArray}
+          id={character.id}
+          key={character.id}
+          image={character.image}
+          name={character.name}
+          onClick={() => console.log(`clicked`)}
+          thisId={() => console.log(character.id)}
           />
-        ))}
+          ))}
       </Wrapper>
     );
   }
 }
+/* TODO
+  if user clicks the same image twice
+  then....
+  set score to 0
+*/
