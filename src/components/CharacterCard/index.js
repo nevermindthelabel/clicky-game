e03f3a76e159d107c8487d50d3bbd3394732e0ab
nevermindthelabel <nevermindthelabel@gmail.com>
2019-03-02 20:47:18 -0700

    added props to character card onclick

--- src/components/CharacterCard/index.js
+++ src/components/CharacterCard/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import './style.css';
 
 export default function CharacterCard(props) {
-  // console.log(props);
   return (
-    <div onClick={props.thisId} className="card">
+    <div onClick={props.shuffleArray} className="card">
       <img 
         src={props.image}
         alt={props.name}
