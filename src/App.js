1e4afa7262b99b4f8e913df8a3b6d83b4baec3a6
Matt Kilcup <nevermindthelabel@gmail.com>
2019-06-08 20:29:09 -0700

    removed unneeded calls from app.js

--- src/App.js
+++ src/App.js
@@ -6,65 +6,63 @@ import "./App.css";
 import characterArray from "./characterArray";
 
 export default class App extends Component {
-
-  constructor() {
-      super();
-      this.state = {
-      characterArray,
-      score: 0,
-      maxScore: 0,
-      clickedArray: [],
-    };
-  }
-
+  state = {
+    characterArray,
+    score: 0,
+    maxScore: 0,
+    clickedArray: []
+  };
 
   handleIncrement = () => {
-    this.setState({ score: this.state.score + 1 })
-      if (this.state.score >= this.state.maxScore) {
-        this.setState({ maxScore: this.state.maxScore + 1 })
-      } 
-  } 
+    this.setState({ score: this.state.score + 1 });
+    if (this.state.score >= this.state.maxScore) {
+      this.setState({ maxScore: this.state.maxScore + 1 });
+    }
+  };
 
   startGame = () => {
     this.setState({
       score: 0,
       maxScore: this.state.maxScore,
       clickedArray: []
-     })
-  }
-  
-  shuffleArray = (id) => {
-    this.setState({ 
+    });
+  };
+
+  shuffleArray = id => {
+    this.setState({
       clickedArray: [...this.state.clickedArray, id]
-     })
-     if (this.state.clickedArray.indexOf(id) !== -1){
-       this.startGame()
-     } else {
+    });
+    if (this.state.clickedArray.indexOf(id) !== -1) {
+      this.startGame();
+    } else {
       this.handleIncrement();
       let lastIndex = characterArray.length - 1;
       for (; lastIndex > 0; lastIndex--) {
-      const randomIndex = Math.floor(Math.random() * (lastIndex + 1));
-      const temp = characterArray[lastIndex];
-      characterArray[lastIndex] = characterArray[randomIndex];
-      characterArray[randomIndex] = temp;
+        const randomIndex = Math.floor(Math.random() * (lastIndex + 1));
+        const temp = characterArray[lastIndex];
+        characterArray[lastIndex] = characterArray[randomIndex];
+        characterArray[randomIndex] = temp;
+      }
+      this.setState({ characterArray });
     }
-    this.setState({ characterArray })
-  }
-  }
+  };
 
   render() {
     return (
       <Wrapper>
-        <Title>The Simpsons Characters
-        <p>score: {this.state.score} Top score: {this.state.maxScore} </p>
-          </Title> 
+        <Title>
+          The Simpsons Characters
+          <p>
+            score: {this.state.score} Top score: {this.state.maxScore}{" "}
+          </p>
+        </Title>
         {this.state.characterArray.map(character => (
           <CharacterCard
-          shuffleArray={this.shuffleArray}
-          id={character.id}
-          key={character.id}
-          image={character.image}
-          name={character.name}
+            shuffleArray={this.shuffleArray}
+            id={character.id}
+            key={character.id}
+            image={character.image}
+            name={character.name}
           />
         ))}
       </Wrapper>
